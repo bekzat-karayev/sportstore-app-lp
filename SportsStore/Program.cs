97f3985a4a62fc8d832170ab3c7992a43044e82b
Bekzat Karayev <bekzatkarayev@gmail.com>
2023-12-12 20:55:36 +0600

    Configure the app

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,33 @@
+/*  The Program.cs file is used to configure the ASP.NET Core application.
+*/
+
 var builder = WebApplication.CreateBuilder(args);
+
+/*  The builder.Services property is used to set up objects, known as services, that can be used throughout
+the application and that are accessed through a DI.
+    The AddControllersWithViews method sets up the shared objects required by applications
+using the MVC Framework and the Razor view engine.
+*/
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+/*  ASP.NET Core receives HTTP requests and passes them along a request pipeline, which is populated
+with middleware components registered using the app property. Each middleware component is able to
+inspect requests, modify them, generate a response, or modify the responses that other components have
+produced. 
+    The UseStaticFiles method enables support for serving static content from the wwwroot folder and
+will be created later in the chapter
+*/
+app.UseStaticFiles();
+/*  One especially important middleware component provides the endpoint routing feature, which
+matches HTTP requests to the application features—known as endpoints—able to produce responses for
+them.  
+    The endpoint routing feature is added to the request
+pipeline automatically, and the MapDefaultControllerRoute registers the MVC Framework as a source of
+endpoints using a default convention for mapping requests to classes and methods.
+*/
+app.MapDefaultControllerRoute();
 
 app.Run();
