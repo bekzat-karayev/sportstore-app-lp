19762d84b1518753990430b71bee8d2cef5358e6
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-01-03 20:09:03 +0600

    Configure Entity Framework

---
+++ SportsStore/Models/StoreDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    /*  The DbContext base class provides access to the Entity Framework Core’s underlying functionality, and
+    the Products property will provide access to the Product objects in the database. 
+        The StoreDbContext class is derived from DbContext and adds the properties that will be used to read and write 
+    the application’s data.
+    */
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) {}
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,5 +1,7 @@
 /*  The Program.cs file is used to configure the ASP.NET Core application.
 */
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +11,14 @@ the application and that are accessed through a DI.
 using the MVC Framework and the Razor view engine.
 */
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(options => {
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
 
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
+
 /*  ASP.NET Core receives HTTP requests and passes them along a request pipeline, which is populated
 with middleware components registered using the app property. Each middleware component is able to
 inspect requests, modify them, generate a response, or modify the responses that other components have
@@ -21,6 +27,7 @@ produced.
 will be created later in the chapter
 */
 app.UseStaticFiles();
+
 /*  One especially important middleware component provides the endpoint routing feature, which
 matches HTTP requests to the application features—known as endpoints—able to produce responses for
 them.  
