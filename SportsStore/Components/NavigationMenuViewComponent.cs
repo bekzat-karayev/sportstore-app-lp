47de46809a5648520ce90023eaa30a35124e34e7
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-01-09 17:30:21 +0600

    Set up navigation view component

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+    /*  The view component’s Invoke method is called when the component is used in a Razor view, and the
+    result of the Invoke method is inserted into the HTML sent to the browser.  
+    */
+    public string Invoke()
+    {
+        return "Hello mazafaka, i am view componentn";
+    }
+}
