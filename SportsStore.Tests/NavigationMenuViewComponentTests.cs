b30b4e28ada2a70320b7ae731742ec7b7d4aa701
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-01-13 02:27:12 +0600

    Pass selected category using viewbag

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Moq;
 using SportsStore.Components;
@@ -34,5 +36,39 @@ public class NavigationMenuViewComponentTests
             "Oranges",
             "Plums"
         }, results));
+    }
+
+    [Fact]
+    public void Indicates_Selected_Category()
+    {
+        // Arrange
+
+        string categoryToSelect = "Apples";
+        Mock<IStoreRepository> mock = new();
+        mock.Setup(m => m.Products).Returns((new Product[] {
+            new Product { ProductID = 1, Name = "P1", Category = "Apples"},
+            new Product { ProductID = 2, Name = "P2", Category = "Oranges"}
+        }).AsQueryable());
+
+        /*  This unit test provides the view component with routing data through the ViewComponentContext
+        property, which is how view components receive all their context data. The ViewComponentContext
+        property provides access to view-specific context data through its ViewContext property, which in
+        turn provides access to the routing information through its RouteData property. 
+        */
+        NavigationMenuViewComponent target = new(mock.Object);
+        target.ViewComponentContext = new ViewComponentContext {
+            ViewContext = new ViewContext {
+                RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+            }
+        };
+        target.RouteData.Values["category"] = categoryToSelect;
+
+        // Act 
+
+        string? result = (string?)(target.Invoke() as ViewViewComponentResult)?.ViewData?["SelectedCategory"];
+
+        // Assert
+
+        Assert.Equal(categoryToSelect, result);
     }
 }
--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -17,9 +17,15 @@ public class NavigationMenuViewComponent : ViewComponent
     }
     /*  The view component’s Invoke method is called when the component is used in a Razor view, and the
     result of the Invoke method is inserted into the HTML sent to the browser.  
+        I use the RouteData property to access the request data to get the value for the currently
+    selected category. I could pass the category to the view by creating another view model class (and that’s
+    what I would do in a real project), but for variety, I am going to use the view bag feature, which allows
+    unstructured data to be passed to a view alongside the view model object.
+        The ViewBag is a dynamic object that allows me to define new properties simply by assigning values to them.
     */
     public IViewComponentResult Invoke()
     {
+        ViewBag.SelectedCategory = RouteData?.Values["category"];
         return View(repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x));
     }
 }
