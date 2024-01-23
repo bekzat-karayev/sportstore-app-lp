904dede27bde29eec7e45e99b530fc74dfaa8021
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-01-23 22:46:02 +0600

    Add OnPost method test

--- SportsStore.Tests/CartPageTests.cs
+++ SportsStore.Tests/CartPageTests.cs
@@ -34,7 +34,7 @@ public class CartPageTests
         testCart.AddItem(p2, 1);
 
         Mock<ISession> mockSession = new();
-        byte[] data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(testCart));
+        byte[]? data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(testCart));
         mockSession.Setup(c => c.TryGetValue(It.IsAny<string>(), out data));
 
         Mock<HttpContext> mockContext = new();
@@ -55,5 +55,44 @@ public class CartPageTests
 
         Assert.Equal(2, cartModel.Cart?.Lines.Count);
         Assert.Equal("myUrl", cartModel.ReturnUrl);
+    }
+
+    [Fact]
+    public void Can_Update_Cart()
+    {
+        // Arrange
+
+        Product p1 = new() { ProductID = 1, Name = "P1"};
+        Mock<IStoreRepository> mockRepository = new();
+        mockRepository.Setup(m => m.Products).Returns((new Product[] { p1 }).AsQueryable<Product>());
+
+        Cart? testCart = new();
+
+        Mock<ISession> mockSession = new();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Callback<string, byte[]>((key, val) => {
+                testCart = JsonSerializer.Deserialize<Cart>(Encoding.UTF8.GetString(val));
+            });
+
+
+        Mock<HttpContext> mockContext = new();
+        mockContext.SetupGet(c => c.Session).Returns(mockSession.Object);
+
+        // Act
+
+        CartModel cartModel = new(mockRepository.Object) {
+            PageContext = new(new ActionContext {
+                HttpContext = mockContext.Object,
+                RouteData = new(),
+                ActionDescriptor = new()
+            })
+        };
+        cartModel.OnPost(1, "myUrl");
+
+        // Assert
+
+        Assert.Single(testCart.Lines);
+        Assert.Equal("P1", testCart.Lines.First().Product.Name);
+        Assert.Equal(1, testCart.Lines.First().Quantity);
     }
 }
