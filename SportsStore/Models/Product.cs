94b8bdf32223adebaf39b3eacdc5fa03dd412f68
Bekzat Karayev <bekzatkarayev@gmail.com>
2024-02-03 23:12:34 +0600

    Add validation to product model

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models
@@ -5,15 +6,23 @@ namespace SportsStore.Models
     public class Product 
     {
         public long? ProductID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; } = String.Empty;
+        
+        [Required(ErrorMessage = "Please enter a product description")]
         public string Description { get; set; } = String.Empty;
 
         /*  This property has been decorated with the Column attribute to specify the SQL data type that
         will be used to store values for this property. Not all C# types map neatly onto SQL types, and this attribute
         ensures the database uses an appropriate type for the application data.  
         */
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a product category")]
         public string Category { get; set; } = String.Empty;
     }
 }
